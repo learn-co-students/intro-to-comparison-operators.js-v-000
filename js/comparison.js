9400e430102e38f0a70039245ac87c6c78bd9895
kylegraydev <kylegraydev@gmail.com>
2016-05-16 15:52:54 -0700

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var first = 1;
+  var second = 2;
+  return first == second;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var first = 1;
+  var second = 2;
+  return first != second;
+}
+
+function testTrueLooseEqualityComparison() {
+  var num = 1;
+  var str = "1";
+  return num == str;
+}
+
+function testFalseStrictEqualityComparison() {
+  var num = 1;
+  var str = "1";
+  return num === str;
+}
+
+function testTrueStrictEqualityComparison() {
+  var num = 1;
+  var str = "1";
+  return num === parseInt(str);
+}
