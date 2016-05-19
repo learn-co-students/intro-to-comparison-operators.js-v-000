56f87d786888f4308cb46e71d5fed0bc3b000957
Tyler Smith <oats000@hotmail.com>
2016-05-18 21:02:14 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison(){
+  var one = 1;
+  var zero = 0;
+  return one == zero;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var one = 1;
+  var zero = 0;
+  return one != zero;
+}
+
+function testTrueLooseEqualityComparison(){
+  var one = 1;
+  var oneS = "1";
+  return one == oneS;
+}
+
+function testFalseStrictEqualityComparison(){
+  var one = 1;
+  var oneS = "1";
+  return one === oneS;
+}
+
+function testTrueStrictEqualityComparison(){
+  var one = 1;
+  var oneS = "1";
+  return one === parseInt(oneS);
+}
