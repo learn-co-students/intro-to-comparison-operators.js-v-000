ddd00ee9f8452313fb50c3d3350ae4953c70debb
Kenneth Amper <kenn.amp@gmail.com>
2016-05-22 21:09:30 -0700

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+	var var0 = 0;
+	var var1 = 1;
+	return var0 == var1;
+}
+
+function testTrueLooseNonEqualityComparison() {
+	var var0 = 0;
+	var var1 = 1;
+	return var0 != var1;
+}
+
+function testTrueLooseEqualityComparison() {
+	var var0 = 1;
+	var var1 = "1";
+	return var0 == var1;
+}
+
+function testFalseStrictEqualityComparison() {
+	var var0 = 1;
+	var var1 = "1";
+	return var0 === var1;
+}
+
+
+function testTrueStrictEqualityComparison() {
+	var var0 = 1;
+	var var1 = "1";
+	return var0 === parseInt(var1);
+}
