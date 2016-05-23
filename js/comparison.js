c64acbb5c80c88d02350d8d6af27e93b51707d3d
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-05-23 13:48:45 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,30 @@
 'use strict';
+function testFalseLooseEqualityComparison(){
+  var zero=0
+  var one=1
+  return zero==one
+}
+
+function testTrueLooseNonEqualityComparison(){
+  var zero=0
+  var one=1
+  return zero!=one
+}
+
+function testTrueLooseEqualityComparison(){
+  var one=1
+  var uno="1"
+  return one==uno
+}
+
+function testFalseStrictEqualityComparison(){
+  var one=1
+  var uno="1"
+  return one!=uno
+}
+
+function testTrueStrictEqualityComparison(){
+  var one=1
+  var uno="1"
+  return one==parseInt(uno)
+}
