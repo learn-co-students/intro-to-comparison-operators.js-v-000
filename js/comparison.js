e546d27ca96a31a092f7673cc55757fd3f82d5ac
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-15 23:31:16 +0000

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,25 @@
 'use strict';
+function testFalseLooseEqualityComparison(){
+  var zero = 0, one = 1;
+  return zero == one;
+}
+
+function testTrueLooseNonEqualityComparison(){
+  var zero = 0, one = 1;
+  return zero != one;
+}
+
+function testTrueLooseEqualityComparison(){
+  var oneNum = 1, oneStr = '1';
+  return oneNum == oneStr;
+}
+
+function testFalseStrictEqualityComparison(){
+  var oneNum = 1, oneStr = '1';
+  return oneNum === oneStr;
+}
+
+function testTrueStrictEqualityComparison(){
+  var oneNum = 1, oneStr = '1';
+  return oneNum === parseInt(oneStr,10);  
+}
