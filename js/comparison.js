c1573f8ef7aa50ec4894cd30e944d9ca3c63e1c1
Christopher Bruen <bruen.chris@gmail.com>
2016-06-15 00:23:42 +0000

    done

--- js/comparison.js
+++ js/comparison.js
@@ -1,3 +1,33 @@
 'use strict';
 
-try;
+function testFalseLooseEqualityComparison() {
+  var one = 0;
+  var two = 1;
+  return one == two;
+};
+
+function testTrueLooseNonEqualityComparison() {
+  var one = 0;
+  var two = 1;
+  return one != two;
+};
+
+function testTrueLooseEqualityComparison() {
+  var one = 1;
+  var two = "1";
+  return one == two;
+}
+
+function testFalseStrictEqualityComparison() {
+var one = 1;
+var two = "1";
+return one === two;
+
+};
+
+function testTrueStrictEqualityComparison() {
+var one = 1;
+var two = "1";
+return one == parseInt(two);
+
+};
