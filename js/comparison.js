864e92d5c68d154f0cdf3bfbd4b8055f10cbfa46
thebrianemory <the@brianemory.com>
2016-07-06 18:39:30 -0700

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var firstValue = 0;
+  var secondValue = 1;
+  return firstValue == secondValue;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var firstValue = 0;
+  var secondValue = 1;
+  return firstValue != secondValue;
+}
+
+function testTrueLooseEqualityComparison() {
+  var firstValue = "1";
+  var secondValue = 1;
+  return firstValue == secondValue;
+}
+
+function testFalseStrictEqualityComparison() {
+  var firstValue = "1";
+  var secondValue = 1;
+  return firstValue === secondValue;
+}
+
+function testTrueStrictEqualityComparison() {
+  var firstValue = "1";
+  var secondValue = 1;
+  return secondValue === parseInt(firstValue);
+}
