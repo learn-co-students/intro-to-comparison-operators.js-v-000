2b2767d2860964d3b2cc5d1e1623a58e1ab7f817
adriandave13 <adrian@paradoxlogic.net>
2016-07-10 15:45:32 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -14,18 +14,18 @@ function testTrueLooseNonEqualityComparison() {
 
 function testTrueLooseEqualityComparison() {
   var one = 1;
-  var one_string = `1`;
+  var one_string = "1";
   return one == one_string;
 }
 
 function testFalseStrictEqualityComparison() {
   var one = 1;
-  var one_string = `1`;
+  var one_string = "1";
   return one === one_string;
 }
 
 function testTrueStrictEqualityComparison() {
   var one = 1;
-  var one_string = `1`;
+  var one_string = "1";
   return one === parseInt(one_string);
 }
