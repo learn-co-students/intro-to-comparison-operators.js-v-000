a3c495c7b07b52508d58bbcf0419c8c2607d1d23
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-11 13:02:30 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var zero = 0;
+  var one  = 1;
+  return zero == one;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var zero = 0;
+  var one  = 1;
+  return zero != one;
+}
