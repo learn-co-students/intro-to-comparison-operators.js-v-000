cc71b4866530965b6284c4dd415028d6780abbe6
Mark Stein <mystycs@gmail.com>
2016-07-22 23:40:09 +0000

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,30 @@
 'use strict';
+function testFalseLooseEqualityComparison() {
+  var x = 0
+  var y = 1
+  return y == x
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var x = 0
+  var y = 1
+  return x != y
+}
+
+function testTrueLooseEqualityComparison() {
+  var x = 1
+  var y = "1"
+  return x == y
+}
+
+function testFalseStrictEqualityComparison() {
+  var x = 1
+  var y = "1"
+  return x === y
+}
+
+function testTrueStrictEqualityComparison() {
+  var x = 1
+  var y = "1"
+  return x === parseInt(y)
+}
