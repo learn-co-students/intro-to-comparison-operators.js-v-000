95b4ed822382421aab053ba9b73bd8e909c0936b
reichardn <reichardn@gmail.com>
2016-07-24 01:25:52 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var v1 = 0;
+  var v2 = 1;
+  return v1 == v2;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var v1 = 0;
+  var v2 = 1;
+  return v1 != v2;
+}
+
+function testTrueLooseEqualityComparison() {
+  var v1 = 1, v2 = "1";
+  return v1 == v2;
+}
+
+function testFalseStrictEqualityComparison() {
+  var v1 = 1, v2 = "1";
+  return v1 === v2;
+}
+
+function testTrueStrictEqualityComparison() {
+  var v1 = 1, v2 = "1";
+  return v1 === parseInt(v2);
+}
