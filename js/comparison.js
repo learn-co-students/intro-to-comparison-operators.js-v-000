148ed3cc176b60106d957b62cf7acaa4c2c821e0
theRingleman <sam.ringleman@gmail.com>
2016-07-25 10:52:06 -0600

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var intOne = 0,
+      intTwo = 1;
+
+  return intOne == intTwo
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var intOne = 0,
+      intTwo = 1;
+
+  return intOne != intTwo
+}
+
+function testTrueLooseEqualityComparison() {
+  var intOne = 1,
+      intTwo = "1";
+
+  return intOne == intTwo
+}
+
+function testFalseStrictEqualityComparison() {
+  var intOne = 1,
+      intTwo = "1";
+
+  return intOne === intTwo
+}
+
+function testTrueStrictEqualityComparison() {
+  var intOne = 1,
+      intTwo = "1";
+
+  return intOne === parseInt(intTwo)
+}
