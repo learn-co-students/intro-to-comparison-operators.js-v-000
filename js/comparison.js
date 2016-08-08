83afcaa1f71b2e63d96167b90891aab087c938fb
JENNIFER YAMADA <jenny.yamada@gmail.com>
2016-08-08 11:58:07 -0700

    done

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var zero = 0;
+  var one = 1;
+  return zero == 1;
+}
+function testTrueLooseNonEqualityComparison() {
+  var zero = 0;
+  var one = 1;
+  return zero != 1;
+}
+function testTrueLooseEqualityComparison() {
+  var st_one = "1";
+  var one = 1;
+  return st_one == one;
+}
+function testFalseStrictEqualityComparison() {
+  var st_one = "1";
+  var one = 1;
+  return st_one === one;
+}
+function testTrueStrictEqualityComparison() {
+  var st_one = "1";
+  var one = 1;
+  return parseInt(st_one) === one;
+}
