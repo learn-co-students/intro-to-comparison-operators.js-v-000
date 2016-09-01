cf29e84f16dfa4134c5e4fa7d31d7baba6061f21
ndaniele <n.daniele@gmail.com>
2016-09-01 19:51:02 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison(){
+  var zero = 0; 
+  var one = 1;
+  return zero == one
+}
+
+function testTrueLooseNonEqualityComparison(){
+  var zero = 0; 
+  var one = 1;
+  return zero != one
+}
+
+function testTrueLooseEqualityComparison(){
+  var string = "1"; 
+  var one = 1;
+  return string == one
+}
+
+function testFalseStrictEqualityComparison(){
+  var string = "1"; 
+  var one = 1;
+  return string === one
+}
+
+function testTrueStrictEqualityComparison(){
+   var string = "1"; 
+  var one = 1;
+  return string !== one
+}
