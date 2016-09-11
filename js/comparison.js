cadc5325d514ed6c0b7567dfe71c16e03abdb1b3
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-11 17:21:07 -0400

    using ubuntu, didn't run tests, run when on mac

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+	var one = 1;
+	var zero = 0;
+	return one == zero; 
+}
+
+function testTrueLooseNonEqualityComparison() {
+	var zero = 0;
+	var one = 1;
+	return zero != one;
+}
+
+function testTrueLooseEqualityComparison() {
+	var string = "1";
+	var num = 1;
+	return num == string;
+} 
+
+function testFalseStrictEqualityComparison() {
+	var string = "1";
+	var num = 1;
+	return string === num;
+}
+
+function testTrueStrictEqualityComparison() {
+	var string = "1";
+	var num = 1;
+	return num === parseInt(string)
+}
