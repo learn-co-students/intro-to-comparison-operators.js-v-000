aaee97fa4f37875ec1ef45f7556d96c9eadf9b9c
gabe <gj+code@mail.co.de>
2016-09-20 01:54:38 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var zeroVariable = 0;
+  var oneVariable = 1;
+  return zeroVariable == oneVariable;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var zeroVariable = 0;
+  var oneVariable = 1;
+  return zeroVariable != oneVariable;
+}
+
+function testTrueLooseEqualityComparison() {
+  var numberVariable = 1;
+  var stringVariable = '1';
+  return numberVariable == stringVariable;
+}
+
+function testFalseStrictEqualityComparison() {
+  var numberVariable = 1;
+  var stringVariable = '1';
+  return numberVariable === stringVariable;
+}
+
+function testTrueStrictEqualityComparison() {
+  var numberVariable = 1;
+  var stringVariable = '1';
+  return numberVariable === parseInt(stringVariable);
+}
