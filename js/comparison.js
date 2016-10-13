d16994786e2cc159f788f2fd1f589dc4f2f19926
DEREJE GEMECHU <dereje88@gmail.com>
2016-10-13 04:34:42 +0000

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var test = (1 == 0) ? true : false;
+  return test;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var test = (1 != 0) ? true : false;
+  return test;
+}
+
+function testTrueLooseEqualityComparison() {
+  var test = ("1" == 1) ? true : false;
+  return test;
+}
+
+function testFalseStrictEqualityComparison() {
+  var test = (1 === "1") ? true : false;
+  return test;
+
+}
+
+function testTrueStrictEqualityComparison() {
+  var test = (1 === parseInt("1")) ? true : false;
+  return test;
+
+}
