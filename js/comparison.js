fbf5b1a9eb4f94a5215059ace813dee33aaadd99
SiamKing <thesiameseelectric@gmail.com>
2016-10-15 15:25:27 -0700

    apples to oranges

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var i = 0;
+  var x = 1;
+  return i == x;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var i = 0;
+  var x = 1;
+  return i != x;
+}
+
+function testTrueLooseEqualityComparison() {
+  var i = "1";
+  var x = 1;
+  return i == x;
+}
+
+function testFalseStrictEqualityComparison() {
+  var i = "1";
+  var x = 1;
+  return i === x;
+}
+
+function testTrueStrictEqualityComparison() {
+  var i = 1;
+  var x = "1";
+  return i === parseInt(x);
+}
