2ead61dbfde2ada831c45c19f83c69d05d0cf472
Logan McAnsh <l.m@loganmcansh.com>
2016-10-16 18:58:04 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var foo = 0,
+      bar = 1;
+
+  return foo == bar;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var foo = 0,
+      bar = 1;
+
+  return foo != bar;
+}
+
+function testTrueLooseEqualityComparison() {
+  var foo = 1,
+      bar = 1;
+
+  return foo == bar;
+}
+
+function testFalseStrictEqualityComparison() {
+  var foo = 1,
+      bar = "1";
+
+  return foo === bar;
+}
+
+function testTrueStrictEqualityComparison() {
+  var foo = 1,
+      bar = "1";
+
+  return foo === parseInt(bar);
+}
