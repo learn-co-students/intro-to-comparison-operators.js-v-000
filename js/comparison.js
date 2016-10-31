2399afc485f91d73ee7374ba53c9090b1aa4022c
github username <github email address>
2016-10-31 13:57:08 -0400

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var one = 1;
+  var two = 2;
+  return one == two;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var one = 1;
+  var two = 2;
+  return one != two;
+}
+
+function testTrueLooseEqualityComparison() {
+  var one = 1 ;
+  var onee = "1";
+  return one == onee;
+}
+
+function testFalseStrictEqualityComparison() {
+  var one = 1;
+  var onee = "1";
+  return one === onee;
+}
+
+function testTrueStrictEqualityComparison() {
+  var one = 1;
+  var onee = "1";
+  return one == parseInt(onee);
+
+}
