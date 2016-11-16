5ee4a05e6d21db079d8eb2d14424bd8213fa04d0
Levio Marotti III <lm3@protonmail.com>
2016-11-16 06:59:10 -0600

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var one = 1;
+  var zero = 0;
+  return one == zero;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var one = 1;
+  var zero = 0;
+  return one != zero;
+}
+
+function testTrueLooseEqualityComparison() {
+  var one = 1;
+  var quotes = "1";
+  return one == quotes;
+}
+
+function testFalseStrictEqualityComparison() {
+  var one = 1;
+  var quotes = "1";
+  return one === quotes;
+}
+
+function testTrueStrictEqualityComparison() {
+  var one = 1;
+  var quotes = "1";
+  return one === parseInt(quotes);
+}
