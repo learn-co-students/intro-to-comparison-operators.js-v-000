1d104f8ee87f7ab9d99cda83ff8dba026b69a515
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-11-20 14:05:30 +0000

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+var a=0;
+var b=1;
+return a==b;
+}
+
+function testTrueLooseEqualityComparison() {
+  var a=0
+  var b=1
+  return a != b
+}
