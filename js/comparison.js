1235646ea6ac16a0e0f56a64cf638c4f5148e9c1
Yechiel K <chilikkal@gmail.com>
2016-12-03 19:59:29 -0500

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var zero = "0"
+  var one = "1"
+  return zero == one
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var zero = "0"
+  var one = "1"
+  return zero != one
+}
+
+function testTrueLooseEqualityComparison() {
+  var string = "1"
+  var integer = 1
+  return string == integer
+}
+
+function testFalseStrictEqualityComparison() {
+  var string = "1"
+  var integer = 1
+  return string === integer
+}
+
+function testTrueStrictEqualityComparison() {
+  var string = "1"
+  var integer = 1
+  return parseInt(string) === integer
+}
