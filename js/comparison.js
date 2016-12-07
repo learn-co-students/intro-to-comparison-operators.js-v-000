b308346f697617e03a66e7bd1237744ce01133f6
lynch16 <will.lynch91@gmail.com>
2016-12-07 18:19:58 -0500

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  return 1 == 0;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  return 1 != 0;
+}
+
+function testTrueLooseEqualityComparison() {
+  return 1 == "1";
+}
+
+function testFalseStrictEqualityComparison() {
+  return 1 === '1';
+}
+
+function testTrueStrictEqualityComparison() {
+  var one = '1';
+  return 1 == one;
+}
