ec810501c4d1cb64bbdd9e2a9cdef7c9b3a9a431
bshap93 <bshap93@gmail.com>
2016-12-10 11:24:28 -0600

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var apple = 0;
+  var orange = 1;
+  return (apple == orange);
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var x = 0;
+  var y = 1;
+  return (x != y);
+}
+
+
+function testTrueLooseEqualityComparison() {
+  var i = 1;
+  var j = "1";
+  return i == j;
+}
+
+function testFalseStrictEqualityComparison() {
+  var i = 1;
+  var j = "1";
+  return i === j;
+}
+
+function testTrueStrictEqualityComparison() {
+  var i = 1;
+  var j = "1";
+  return i === parseInt(j);
+}
