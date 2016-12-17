f282a7489900ed97deda1c3bed051f792ec2c4c8
lcorr8 <l.corr.v@gmail.com>
2016-12-17 16:33:03 -0500

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var one = 1;
+  var zero = 0;
+  return one == zero;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var one = 1;
+  var zero = 0;
+  return one != zero;
+}
+
+function testTrueLooseEqualityComparison() {
+  var one = "1";
+  var onenumber = 1;
+  return one == onenumber;
+}
+
+function testFalseStrictEqualityComparison() {
+  var one = "1";
+  var onenumber = 1;
+  return one === onenumber;
+}
+
+function testTrueStrictEqualityComparison() {
+  var one = "1";
+  var onenumber = 1;
+  return parseInt(one) === onenumber;  
+}
