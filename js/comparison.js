99916a6390b85765d90fbcbcb9e20c132f7266f8
Clare <clare.e.horton@gmail.com>
2016-12-20 01:15:51 -0600

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var one = 1;
+  var zero = 0;
+  return one == zero;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var one = 1;
+  var zero = 0;
+  return one != zero;
+}
+
+function testTrueLooseEqualityComparison() {
+  var number = 1;
+  var string = "1";
+  return number == string;
+}
+
+function testFalseStrictEqualityComparison() {
+  var number = 1;
+  var string = "1";
+  return number === string;
+}
+
+function testTrueStrictEqualityComparison() {
+  var number = 1;
+  var string = "1";
+  return number === parseInt(string);
+}
