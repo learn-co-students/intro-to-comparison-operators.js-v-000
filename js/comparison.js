23778b952b2d4983c8e859b0d47895d811080d00
BrunoBoehm <bruno.boehm@gmail.com>
2017-01-10 14:58:14 +0100

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+	var zero = 0;
+	var one = 1;
+	return zero == one;
+}
+
+function testTrueLooseNonEqualityComparison() {
+	var zero = 0;
+	var one = 1;
+	return zero != one;
+}
+
+function testTrueLooseEqualityComparison() {
+	var one = 1;
+	var onetext = "1";
+	return one == onetext;
+}
+// will return true
+
+function testFalseStrictEqualityComparison() {
+	var one = 1;
+	var onetext = "1";
+	return one === onetext;
+}
+// will return false
+
+function testTrueStrictEqualityComparison() {
+	var one = 1;
+	var onetext = "1";
+	return one === parseInt(onetext);
+}
