14428814ce43bb264d1b5c6618d65ce490ef4d30
bmolina-nyc <bruce.molina.81@gmail.com>
2017-01-14 12:43:31 -0500

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function testFalseLooseEqualityComparison(){
+  var variableOne = 0;
+  var variableTwo = 1;
+  return variableOne == variableTwo;
+}
+
+function testTrueLooseNonEqualityComparison(){
+  var variableOne = 0;
+  var variableTwo = 1;
+  return variableOne != variableTwo;
+}
+
+function testTrueLooseEqualityComparison(){
+  var variableOne = 1;
+  var variableTwo = "1";
+  return variableOne == variableTwo;
+}
+// returns true because the "==" above is a 'loose' comaprison operator
+
+
+function testFalseStrictEqualityComparison(){
+  var variableOne = 1;
+  var variableTwo = "1";
+  return variableOne === variableTwo;
+}
+
+function testTrueStrictEqualityComparison(){
+  var variableOne = 1;
+  var variableTwo = "1"; 
+  return variableOne === parseInt(variableTwo, 10)
+}
