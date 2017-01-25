3c9278771e03b4e61ff47381dea5c3d7458e4e56
Bill <storywr@gmail.com>
2017-01-25 16:37:57 -0500

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var apple = 0
+  var orange = 1
+  return apple == orange
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var pear = 0
+  var strawberry = 1
+  return pear != strawberry
+}
+
+function testTrueLooseEqualityComparison() {
+  var blueberry = 1
+  var grapefruit = "1"
+  return blueberry == grapefruit
+}
+
+function testFalseStrictEqualityComparison() {
+  var blueberry = 1
+  var grapefruit = "1"
+  return blueberry === grapefruit
+}
+
+function testTrueStrictEqualityComparison() {
+  var blueberry = 1
+  var grapefruit = "1"
+  return blueberry === parseInt(grapefruit)
+}
