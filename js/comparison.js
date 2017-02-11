ecc5a3a213d81b390aa01ab31502966d2ed3faa6
Jennifer Haddock <jentx123@hotmail.com>
2017-02-11 04:12:43 +0000

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+var x = 0;
+var y = 1;
+return x == y;  
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var a = 0;
+  var b = 1;
+  return a != b;
+}
+
+function testTrueLooseEqualityComparison() {
+  var c = 1;
+  var d = '1';
+  return c == d;
+}
+
+function testFalseStrictEqualityComparison() {
+  var m = 1;
+  var n = "1";
+  return m === n;
+}
+
+function testTrueStrictEqualityComparison() {
+  var r = 1;
+  var s = "1";
+  return r !== s;
+}
