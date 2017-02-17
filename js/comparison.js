4d507a3dbdbfb0a2207b69e1cdd73685c3bb5e5b
msosborne3 <msosborne@ymail.com>
2017-02-17 12:16:52 -0600

    Done.

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function testFalseLooseEqualityComparison() {
+  var num1 = 0;
+  var num2 = 1;
+  return num1 == num2;
+}
+
+function testTrueLooseNonEqualityComparison() {
+  var num1 = 0;
+  var num2 = 1;
+  return num1 != num2;
+}
+
+function testTrueLooseEqualityComparison() {
+  var num1 = 1;
+  var string1 = "1";
+  return num1 == string1;
+}
+
+function testFalseStrictEqualityComparison() {
+  var num1 = 1;
+  var string1 = "1";
+  return num1 === string1;
+}
+
+function testTrueStrictEqualityComparison() {
+  var num1 = 1;
+  var string1 = "1";
+  return num1 === parseInt(string1);
+}
